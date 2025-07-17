854fa8f7c1fcd3f411453a393d24be0ccdb28fae
Ucokgreget <luqmanalfauzan1738@gmail.com>
2025-07-17 17:31:20 +0700

    revisi home

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -2,16 +2,16 @@ import { Leaf } from "lucide-react";
 
 const About = () => {
   return (
-    <section className="bg-emerald-100 py-12">
+    <section className="bg-emerald-800 text-white py-16 px-4">
       <div className="max-w-4xl mx-auto text-center">
         <div className="flex items-center justify-center mb-4">
-          <Leaf className="w-8 h-8 text-green animate-pulse" />
+          <Leaf className="w-8 h-8 text-white animate-pulse" />
         </div>
 
-        <h2 className="text-green-900 arabic-font text-2xl mb-4 text-3xl md:text-4xl font-bold mb-2">Apa itu EcoMasjid?</h2>
-        <div className="w-20 h-1 bg-green-500 mx-auto mb-6 rounded-full"></div>
+        <h2 className="text-3xl md:text-4xl font-bold mb-2">Apa itu EcoMasjid?</h2>
+        <div className="w-20 h-1 bg-white mx-auto mb-6 rounded-full"></div>
 
-        <p className="text-lg md:text-xl leading-relaxed text-green/90 max-w-2xl mx-auto">
+        <p className="text-lg md:text-xl leading-relaxed text-white/90 max-w-2xl mx-auto">
           <strong>EcoMasjid</strong> adalah platform digital yang mendorong masjid untuk menerapkan prinsip ramah lingkungan berdasarkan ajaran Islam. Kami percaya bahwa masjid bisa menjadi pusat edukasi dan aksi lingkungan yang berkelanjutan.
         </p>
       </div>
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,22 +2,25 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
   return (
-    <section class="relative bg-green-700 text-white py-20 overflow-hidden">
+    <section class="relative bg-white text-white py-20 overflow-hidden">
     <div class="absolute inset-0 leaf-pattern opacity-10"></div>
     <div class="container mx-auto px-4 flex flex-col md:flex-row justify-between items-center">
         <div class="md:w-1/2 mb-4">
-            <h2 class="text-3xl font-bold mb-4 leading-tight">
-                EcoMasjid: Gerakan Masjid <span class="text-emerald-300">Ramah Lingkungan</span>
+            <h2 className="text-2xl md:text-5xl font-extrabold text-emerald-800 mb-4 leading-tight text-balance">
+                EcoMasjid: Gerakan Masjid Ramah Lingkungan
             </h2>
-            <p class="text-lg">
+            <p class="ext-base md:text-lg text-gray-700 mb-6 leading-relaxed text-balance">
               Menginspirasi masjid untuk hidup berkelanjutan berdasarkan nilai-nilai Islam:
               kebersihan, amanah, dan tanggung jawab terhadap Bumi
             </p>
-            <div class="flex space-x-4 mt-4">
-                <button class="bg-emerald-500 hover:bg-emerald-600 text-white px-6 py-3 rounded-lg font-medium transition shadow-md">
-                    Get Started
-                </button>
-            </div>
+            <div className="flex space-x-4 mt-4">
+          <Link
+            to="/masjid"
+            className="bg-emerald-800 text-white px-6 py-2 rounded hover:bg-emerald-900 transition-all"
+          >
+            Lihat Masjid
+          </Link>
+        </div>
         </div>
         <div class="md:w-1/2">
             <img 
