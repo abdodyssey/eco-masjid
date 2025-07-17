218d3d5d912d2208f54a26fd1980ad4d02163192
bindin <l3m1j0j0@gmail.com>
2025-07-17 19:30:16 +0700

    menambah scroll top button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,31 +12,33 @@ import PengajuanMasjidForm from "./pages/PengajuanMasjidForm";
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
 import AddEvent from "./pages/AddEvent";
+import ScrollToTopButton from "./components/ScrollToTopButton";
 
 function App() {
   return (
-    <>
-      <div className="w-full mx-auto bg-white">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/masjid" element={<MasjidList />} />
-          <Route path="/artikel" element={<ArtikelList />} />
-          <Route path="/kampanye" element={<KampanyeForm />} />
-          <Route path="/kalkulator" element={<KalkulatorKarbon />} />
-          <Route path="/artikel/:id" element={<ArtikelDetail />} />
-          <Route path="/event" element={<EventList />} />
-          <Route path="/pengajuan" element={<PengajuanMasjidForm />} />
+      <>
+        <div className="w-full mx-auto bg-white">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/masjid" element={<MasjidList />} />
+            <Route path="/artikel" element={<ArtikelList />} />
+            <Route path="/kampanye" element={<KampanyeForm />} />
+            <Route path="/kalkulator" element={<KalkulatorKarbon />} />
+            <Route path="/artikel/:id" element={<ArtikelDetail />} />
+            <Route path="/event" element={<EventList />} />
+            <Route path="/pengajuan" element={<PengajuanMasjidForm />} />
+            <Route path="/admin/event/tambah" element={<AddEvent />} />
+            <Route path="/admin/login" element={<AdminLogin />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+          </Routes>
+          <Footer />
+        </div>
 
-          <Route path="/admin/event/tambah" element={<AddEvent />} />
 
-          <Route path="/admin/login" element={<AdminLogin />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-        </Routes>
-        <Footer />
-      </div>
-    </>
+        <ScrollToTopButton />
+      </>
   );
 }
 
